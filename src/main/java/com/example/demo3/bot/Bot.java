2408060e74626b75ed12c33e40c5ba01fb443dd2
Sergei <sk_developer@mail.ru>
2023-01-19 01:28:14 +0300

    simple telegram bot added

---
+++ src/main/java/com/example/demo3/bot/Bot.java
@@ -0,0 +1,62 @@
+package com.example.demo3.bot;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+@Slf4j
+public class Bot extends TelegramLongPollingBot {
+
+    final BotConfig config;
+
+    public Bot(BotConfig config) {
+        this.config = config;
+    }
+
+    public void onUpdateReceived(Update update) {
+        update.getUpdateId();
+        SendMessage.SendMessageBuilder builder = SendMessage.builder();
+        String messageText;
+        String chatId;
+        if (update.getMessage() != null) {
+            chatId = update.getMessage().getChatId().toString();
+            builder.chatId(chatId);
+            messageText = update.getMessage().getText();
+        } else {
+            chatId = update.getChannelPost().getChatId().toString();
+            builder.chatId(chatId);
+            messageText = update.getChannelPost().getText();
+        }
+
+        if (messageText.contains("/hello")) {
+            builder.text("Привет");
+            try {
+                execute(builder.build());
+            } catch (TelegramApiException e) {
+                log.debug(e.toString());
+            }
+        }
+
+        if (messageText.contains("/chartId")) {
+            builder.text("ID Канала : " + chatId);
+            try {
+                execute(builder.build());
+            } catch (TelegramApiException e) {
+                log.debug(e.toString());
+            }
+        }
+    }
+
+
+    public String getBotUsername() {
+        return config.getBotUserName();
+    }
+
+    public String getBotToken() {
+        return config.getToken();
+    }
+}
---
+++ src/main/java/com/example/demo3/bot/BotInitializer.java
@@ -0,0 +1,28 @@
+package com.example.demo3.bot;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@Component
+@Slf4j
+public class BotInitializer {
+    @Autowired
+    Bot bot;
+
+    @EventListener({ContextRefreshedEvent.class})
+    public void Init() throws TelegramApiException {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        try {
+            telegramBotsApi.registerBot(bot);
+        } catch (TelegramApiRequestException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
