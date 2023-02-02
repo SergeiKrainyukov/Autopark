7d72fbe71ffd2cf5e5ce20669a2fd2263f435f1a
Sergei <sk_developer@mail.ru>
2023-02-02 16:04:13 +0300

    bot refactor

--- src/main/java/com/example/demo3/bot/Bot.java
+++ src/main/java/com/example/demo3/bot/Bot.java
@@ -58,126 +58,141 @@ public class Bot extends TelegramLongPollingBot {
                 builder.chatId(chatId);
                 messageText = update.getChannelPost().getText();
             }
-
             if (isAuthorized) {
-                switch (messageText) {
-                    case "/login": {
-                        builder.text("You have been authorized earlier as " + manager.getUsername());
-                        execute(builder.build());
-                    }
-                    case "/vehicle_mileage": {
-                        builder.text("Enter the period, state number and date in format: [period:state_number:date]." +
-                                "\nFor day: DAY:123456:02.02.2022." +
-                                "\nFor month: MONTH:123456:02.02.2022-02.03.2022.");
-                        execute(builder.build());
-                    }
-                }
-                if (messageText.contains(":")) {
-                    String[] splittedMessage = messageText.split(":");
-                    if (splittedMessage.length != 3) {
-                        builder.text("Wrong data format. Try again.");
-                        execute(builder.build());
-                        return;
-                    }
-                    ReportPeriod reportPeriod = calculateReportPeriodFromString(splittedMessage[0]);
-                    if (reportPeriod == null) {
-                        builder.text("Wrong period format. Try again.");
-                        execute(builder.build());
-                        return;
-                    }
-                    int stateNumber = Integer.parseInt(splittedMessage[1]);
-                    switch (reportPeriod) {
-                        case DAY: {
-                            SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
-                            try {
-                                Date dateTo = sdf.parse(splittedMessage[2]);
-                                Calendar calendar = Calendar.getInstance();
-                                calendar.setTime(dateTo);
-                                calendar.add(Calendar.DATE, -1);
-                                Date dateFrom = calendar.getTime();
-                                VehicleEntity vehicleEntity = vehiclesRepository.findVehicleByStateNumber(stateNumber);
-                                if (vehicleEntity == null) {
-                                    builder.text("Vehicle not found. Try again.");
-                                    execute(builder.build());
-                                    return;
-                                }
-                                List<ReportResult> reportResultList = new MileageByPeriodReport(ReportPeriod.DAY, dateFrom.getTime(), dateTo.getTime(), vehicleEntity.getId(), tripRepository).getResult();
-                                int mileageByDay = 0;
-                                for (ReportResult reportResult : reportResultList) {
-                                    mileageByDay += Integer.parseInt(reportResult.getValue());
-                                }
-                                builder.text("Mileage for " + splittedMessage[2] + ": " + mileageByDay + " km");
-                                execute(builder.build());
-                            } catch (ParseException e) {
-                                builder.text("Wrong date format. Try again.");
-                                execute(builder.build());
-                                return;
-                            }
-                        }
-                        case MONTH: {
-                            String[] dates = splittedMessage[2].split("-");
-                            SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
-                            try {
-                                Date dateFrom = sdf.parse(dates[0]);
-                                Date dateTo = sdf.parse(dates[1]);
-                                VehicleEntity vehicleEntity = vehiclesRepository.findVehicleByStateNumber(stateNumber);
-                                if (vehicleEntity == null) {
-                                    builder.text("Vehicle not found. Try again.");
-                                    execute(builder.build());
-                                    return;
-                                }
-                                List<ReportResult> reportResultList = new MileageByPeriodReport(ReportPeriod.DAY, dateFrom.getTime(), dateTo.getTime(), vehicleEntity.getId(), tripRepository).getResult();
-                                double mileageByPeriod = 0;
-                                for (ReportResult reportResult : reportResultList) {
-                                    mileageByPeriod += Double.parseDouble(reportResult.getValue());
-                                }
-                                builder.text("Mileage for " + dates[0] + "-" + dates[1] + ": " + mileageByPeriod + " km");
-                                execute(builder.build());
-                            } catch (ParseException e) {
-                                builder.text("Wrong date format. Try again.");
-                                execute(builder.build());
-                            }
-                        }
-                    }
+                authorizedLogic(messageText, builder);
+                return;
+            }
+            unAuthorizedLogic(messageText, builder);
+        } catch (Exception e) {
+            log.debug(e.toString());
+        }
+    }
 
+    private void authorizedLogic(String messageText, SendMessage.SendMessageBuilder builder) throws Exception {
+        if (messageText.contains(":")) {
+            proceedMileageReportLogic(messageText, builder);
+            return;
+        }
+        proceedCommands(messageText, builder);
+    }
+
+    private void unAuthorizedLogic(String messageText, SendMessage.SendMessageBuilder builder) throws Exception {
+        switch (messageText) {
+            case "/login": {
+                if (manager.getUsername().isBlank()) {
+                    builder.text("Write login:");
+                    execute(builder.build());
+                    return;
+                } else if (manager.getPassword().isBlank()) {
+                    builder.text("Write password:");
+                    execute(builder.build());
+                    return;
                 }
-            } else {
-                switch (messageText) {
-                    case "/login": {
-                        if (manager.getUsername().isBlank()) {
-                            builder.text("Write login:");
-                            execute(builder.build());
-                            return;
-                        } else if (manager.getPassword().isBlank()) {
-                            builder.text("Write password:");
-                            execute(builder.build());
-                            return;
-                        }
-                    }
-                    default: {
-                        if (manager.getUsername().isBlank()) {
-                            manager = new ManagerEntity();
-                            manager.setUsername(messageText);
-                            builder.text("Write password:");
-                            execute(builder.build());
-                        } else if (manager.getPassword().isBlank()) {
-                            manager.setPassword(messageText);
-                            if (validateCredentials()) {
-                                isAuthorized = true;
-                                builder.text("You are successfully authorized");
-                                execute(builder.build());
-                            } else {
-                                isAuthorized = false;
-                                manager = new ManagerEntity();
-                                builder.text("Authorization failed. Write login:");
-                                execute(builder.build());
-                            }
-                        }
+            }
+            default: {
+                if (manager.getUsername().isBlank()) {
+                    manager = new ManagerEntity();
+                    manager.setUsername(messageText);
+                    builder.text("Write password:");
+                    execute(builder.build());
+                } else if (manager.getPassword().isBlank()) {
+                    manager.setPassword(messageText);
+                    if (validateCredentials()) {
+                        isAuthorized = true;
+                        builder.text("You are successfully authorized");
+                        execute(builder.build());
+                    } else {
+                        isAuthorized = false;
+                        manager = new ManagerEntity();
+                        builder.text("Authorization failed. Write login:");
+                        execute(builder.build());
                     }
                 }
             }
-        } catch (Exception e) {
-            log.debug(e.toString());
+        }
+    }
+
+    private void proceedCommands(String messageText, SendMessage.SendMessageBuilder builder) throws Exception {
+        switch (messageText) {
+            case "/login": {
+                builder.text("You have been authorized earlier as " + manager.getUsername());
+                execute(builder.build());
+            }
+            case "/vehicle_mileage": {
+                builder.text("Enter the period, state number and date in format: [period:state_number:date]." +
+                        "\nFor day: DAY:123456:02.02.2022." +
+                        "\nFor month: MONTH:123456:02.02.2022-02.03.2022.");
+                execute(builder.build());
+            }
+        }
+    }
+
+    private void proceedMileageReportLogic(String messageText, SendMessage.SendMessageBuilder builder) throws Exception {
+        String[] splittedMessage = messageText.split(":");
+        if (splittedMessage.length != 3) {
+            builder.text("Wrong data format. Try again.");
+            execute(builder.build());
+            return;
+        }
+        ReportPeriod reportPeriod = calculateReportPeriodFromString(splittedMessage[0]);
+        if (reportPeriod == null) {
+            builder.text("Wrong period format. Try again.");
+            execute(builder.build());
+            return;
+        }
+        int stateNumber = Integer.parseInt(splittedMessage[1]);
+        switch (reportPeriod) {
+            case DAY: {
+                SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+                try {
+                    Date dateTo = sdf.parse(splittedMessage[2]);
+                    Calendar calendar = Calendar.getInstance();
+                    calendar.setTime(dateTo);
+                    calendar.add(Calendar.DATE, -1);
+                    Date dateFrom = calendar.getTime();
+                    VehicleEntity vehicleEntity = vehiclesRepository.findVehicleByStateNumber(stateNumber);
+                    if (vehicleEntity == null) {
+                        builder.text("Vehicle not found. Try again.");
+                        execute(builder.build());
+                        return;
+                    }
+                    List<ReportResult> reportResultList = new MileageByPeriodReport(ReportPeriod.DAY, dateFrom.getTime(), dateTo.getTime(), vehicleEntity.getId(), tripRepository).getResult();
+                    int mileageByDay = 0;
+                    for (ReportResult reportResult : reportResultList) {
+                        mileageByDay += Integer.parseInt(reportResult.getValue());
+                    }
+                    builder.text("Mileage for " + splittedMessage[2] + ": " + mileageByDay + " km");
+                    execute(builder.build());
+                } catch (ParseException e) {
+                    builder.text("Wrong date format. Try again.");
+                    execute(builder.build());
+                    return;
+                }
+            }
+            case MONTH: {
+                String[] dates = splittedMessage[2].split("-");
+                SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+                try {
+                    Date dateFrom = sdf.parse(dates[0]);
+                    Date dateTo = sdf.parse(dates[1]);
+                    VehicleEntity vehicleEntity = vehiclesRepository.findVehicleByStateNumber(stateNumber);
+                    if (vehicleEntity == null) {
+                        builder.text("Vehicle not found. Try again.");
+                        execute(builder.build());
+                        return;
+                    }
+                    List<ReportResult> reportResultList = new MileageByPeriodReport(ReportPeriod.DAY, dateFrom.getTime(), dateTo.getTime(), vehicleEntity.getId(), tripRepository).getResult();
+                    double mileageByPeriod = 0;
+                    for (ReportResult reportResult : reportResultList) {
+                        mileageByPeriod += Double.parseDouble(reportResult.getValue());
+                    }
+                    builder.text("Mileage for " + dates[0] + "-" + dates[1] + ": " + mileageByPeriod + " km");
+                    execute(builder.build());
+                } catch (ParseException e) {
+                    builder.text("Wrong date format. Try again.");
+                    execute(builder.build());
+                }
+            }
         }
     }
 
